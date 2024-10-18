8011fe65e60ad5e3476105e1bc88fdc127a3b789
Amna555673 <149059046+Amna555673@users.noreply.github.com>
2024-10-18 18:55:00 +0500

    Update FacebookCommentSteps.java
    resolve conflict

--- src/main/java/stepDefination/FacebookCommentSteps.java
+++ src/main/java/stepDefination/FacebookCommentSteps.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import base.BaseTest;
+
 import java.time.Duration;
 
 import io.cucumber.java.en.Given;
@@ -15,19 +17,12 @@ import io.cucumber.java.en.When;
 
 public class FacebookCommentSteps extends BaseTest{
 
-public class FacebookCommentSteps {
-
-    WebDriver driver;
-
+    
     WebDriverWait wait;
 
     @Given("I am logged into Facebook")
     public void i_am_logged_into_facebook() {
-
     	initializeDriver("chrome");
-
-        driver = new ChromeDriver();
-
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         driver.get("https://www.facebook.com/");
@@ -35,13 +30,11 @@ public class FacebookCommentSteps {
         driver.findElement(By.id("pass")).sendKeys("ayesha123@");
         driver.findElement(By.name("login")).click();
 
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@aria-label='Facebook']")));
     }
 
     @Given("I see a post on my timeline")
     public void i_see_a_post_on_my_timeline() {
-
         WebElement post = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@data-pagelet, 'FeedUnit')]")));
         if (post.isDisplayed()) {
             System.out.println("Post is visible on the timeline.");
@@ -50,7 +43,6 @@ public class FacebookCommentSteps {
 
     @When("I write a comment on the post")
     public void i_write_a_comment_on_the_post() {
-
         WebElement commentBox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@aria-label,'Write a comment')]")));
         commentBox.click();
         commentBox.sendKeys("This is an automated comment!");
@@ -58,24 +50,18 @@ public class FacebookCommentSteps {
 
     @When("I click the comment button")
     public void i_click_the_comment_button() {
-
         WebElement commentBox = driver.findElement(By.xpath("//div[contains(@aria-label,'Write a comment')]"));
         commentBox.sendKeys("\n");
     }
 
     @Then("my comment should appear under the post")
     public void my_comment_should_appear_under_the_post() {
-
         WebElement comment = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'This is an automated comment!')]")));
         if (comment.isDisplayed()) {
             System.out.println("Comment successfully posted.");
         } else {
             System.out.println("Comment was not posted.");
         }
-
         driver.quit(); 
-
-        driver.quit(); 
-
     }
 }
